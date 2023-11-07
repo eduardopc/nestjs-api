da2ff26166fc37353bf95bfed720db7205b89d95
Eduardo Pacheco <eduardo.celeste@pipefy.com>
2023-11-07 01:29:46 -0300

    feat: handling with private routes + categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
   controllers: [],
   providers: [
     {
--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
+import { CategoriesRepository } from './repositories/categories.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
-  exports: [UsersRepository], // para deixar os métodos visíveis a outros módulos
+  providers: [PrismaService, UsersRepository, CategoriesRepository],
+  exports: [UsersRepository, CategoriesRepository], // para deixar os métodos visíveis a outros módulos
 })
 export class DatabaseModule {}
